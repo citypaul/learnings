17b508bcc6163472f3a05d1de80969cfda80ae82
Paul Hammond <paul.hammond@gmail.com>
2026-06-25 10:40:32 +0100

    more stuff

--- learning/terraform-official-tutorials/aws/main.tf
+++ learning/terraform-official-tutorials/aws/main.tf
@@ -15,9 +15,9 @@ data "aws_ami" "ubuntu" {
 
 resource "aws_instance" "app_server" {
   ami           = data.aws_ami.ubuntu.id
-  instance_type = "t2.micro"
+  instance_type = "t3.micro"
 
   tags = {
-    Name = "learn-terraform"
+    Name = "learn-terraform-changed"
   }
 }
